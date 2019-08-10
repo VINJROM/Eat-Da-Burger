1ddb2f259c62b600f84eff141f5e300484454fe1
Vincent Romine <vinjrom@live.com>
2019-08-10 14:32:05 -0500

    connection for jaws

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,32 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "db"
 });
+
+// JAWSDB connection.
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'db'
+    })
+}
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
